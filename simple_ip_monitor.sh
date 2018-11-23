83566aa545f065c5038001fe0a41667ef38af582
lchabolla <lchabolla@gmail.com>
2018-11-22 17:53:30 -0800

    Fixed bug

--- simple_ip_monitor.sh
+++ simple_ip_monitor.sh
@@ -1,21 +1,27 @@
 #! /bin/bash
 # Author: lchabolla@gmail.com
-# The goal of this script is to ping an IP and log any failures with a timestamp
+
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-localip=$1
+localip="$1"
 
-if [[$1 -eq 0]]
-then
+if [[ "$1" = 0 ]]; then
   echo "Error: Please enter valid IP address."
   exit 1;
-elif
+else
+  echo "Pinging $1"
+fi
+
 /sbin/ping -c 1 $1
+
 if [ $? -eq 0 ]
 then
-echo "$timestamp: up" >> uptime.txt
+echo "Host is up. Appending to log."
+echo "$timestamp: $1 is up." >> uptime.txt
+exit 0;
 elif [ $? -ne 0 ]
 then
-echo "$timestamp: down" >> downtime.txt
-exit 1;
+  echo "Host is down. Appending to log."
+  echo "$timestamp: $1 down." >> downtime.txt
+  exit 1;
 fi
